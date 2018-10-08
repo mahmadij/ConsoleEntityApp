c04f5f811fcb117414445bd0567d08bcff8223e9
Mehrnaz Ahmadi Joobaneh <mahmadij@gmail.com>
2018-10-07 22:36:44 -0400

    first commit

--- ConsoleEntityApp/Program.cs
+++ ConsoleEntityApp/Program.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace ConsoleEntityApp
 {
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
