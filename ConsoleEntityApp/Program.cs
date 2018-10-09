39216374532547ad099fcb8ac6c5adab6aca1926
Mehrnaz Ahmadi Joobaneh <mahmadij@gmail.com>
2018-10-08 22:35:27 -0400

    LINQ and Extension Methods

--- ConsoleEntityApp/Program.cs
+++ ConsoleEntityApp/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace ConsoleEntityApp
+﻿using ConsoleEntityApp.Context;
+using System.Linq;
+using System;
+namespace ConsoleEntityApp
 {    
 
     
@@ -6,6 +9,28 @@
     {
         static void Main(string[] args)
         {
+            var context = new MyDbContext();
+            //LINQ 
+            var query =
+                from c in context.Items
+                where c.Name.Contains("3")
+                orderby c.Name
+                select c;
+
+            foreach(var item in query)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            //extension method
+            var tags = context.Tags
+                .Where(t => t.Name.Contains("Tag"))
+                .OrderBy(c => c.Name);
+
+            foreach (var tag in tags)
+            {
+                Console.WriteLine(tag.Name);
+            }
         }
     }
 }
